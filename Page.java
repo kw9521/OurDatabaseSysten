25fd5c338469eacc720fb07fa57f57f18bdc2f60
nso3075 <nso3075@rit.edu>
2025-01-23 10:19:14 -0500

    Fixing main file, added page class file

---
+++ Page.java
@@ -0,0 +1,15 @@
+public class Page {
+    static int page_size;
+
+    public Page(){
+        
+    }
+}
+
+
+// Possible datatypes:
+// - Integer
+// – Double
+// – Boolean
+// – Char(N)
+// – Varchar(N) (up to N chars)
