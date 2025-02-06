e5ca4e5e04b2f618cad245824bd908cf07a3072f
Kunlin Wen <allis@windows-laptop.local>
2025-02-06 10:29:16 -0500

    catalog parsing written in english

--- src/Catalog.java
+++ src/Catalog.java
@@ -26,4 +26,39 @@ public class Catalog {
     public int getBufferSize(){
         return this.bufferSize;
     }
+
+    // catalog format: ["PS", "numOfTable", 
+    // "Table1ID", "lengthOfTable1Name", "TableName1", "NumOfAttributeinTable1", 
+        // "Len of Att1 Name", "Att1 Name", "datatype", "bitmap is a string of 01s", 
+        // "Len of Att2 Name", "Att2 Name", "datatype", "bitmap is a string of 01s",
+        // ...
+        // "Len of AttN Name", "AttN Name", "datatype", "bitmap is a string of 01s",
+    // "Table2ID", "lengthOfTable2Name", "TableName2", "NumOfAttributeinTable2"]
+        // "Len of Att1 Name", "Att1 Name", "datatype", "bitmap is a string of 01s", 
+        // "Len of Att2 Name", "Att2 Name", "datatype", "bitmap is a string of 01s",
+        // ...
+        // "Len of AttN Name", "AttN Name", "datatype", "bitmap is a string of 01s",
+    // ...
+        // "Len of Att1 Name", "Att1 Name", "datatype", "bitmap is a string of 01s", 
+        // "Len of Att2 Name", "Att2 Name", "datatype", "bitmap is a string of 01s",
+        // ...
+        // "Len of AttN Name", "AttN Name", "datatype", "bitmap is a string of 01s"]
+    public void parseCatalog(ArrayList<String> catalog){
+        for (int i=0; i< catalog.size(); i++){  
+            if (i == 0) {
+                // index 0 = pageSIze
+                this.pageSize = Integer.parseInt(catalog.get(i));
+            } else if (i == 1) {
+                // index 1 = # of tables
+                this.tableCount = Integer.parseInt(catalog.get(i));
+            } else {
+                // rest of the arraylist are tables 
+            }
+
+        }
+
+
+    }
+
+
 }
