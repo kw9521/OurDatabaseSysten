f46dc8a7479fea2e9541e7e2ba92390cd53a18b2
nso3075 <nso3075@rit.edu>
2025-02-20 11:41:03 -0500

    modified page PageBuffer to move hardware ops to storage manager

--- src/PageBuffer.java
+++ src/PageBuffer.java
@@ -7,10 +7,11 @@ import java.util.Map;
 public class PageBuffer {
     private final int capacity;
     private final LinkedHashMap<PageKey, Page> pages;
+    private final StorageManager storageManager = Main.getStorageManager();
 
     public PageBuffer(int capacity) {
         this.capacity = capacity;
-        this.pages = new LinkedHashMap<>() {
+        this.pages = new LinkedHashMap<>() { // LRU cache -- called automatically when something is added
             @Override
             protected boolean removeEldestEntry(Map.Entry<PageKey, Page> eldest) {
                 if (size() > PageBuffer.this.capacity) {
@@ -35,57 +36,65 @@ public class PageBuffer {
     }
 
     public void writePageToHardware(Page page) {
-        if (!page.isUpdated()) return;
+        storageManager.writePage(page); // need to implement this in StorageManager
+
+
+        // Move this functionality to StorageManager
+        // if (!page.isUpdated()) return;
         
-        try (RandomAccessFile fileOut = new RandomAccessFile(Main.getDBLocation() + 
-                "/tables/" + page.getTableId() + ".bin", "rw")) {
-            Table table = Main.getCatalog().getTable(page.getTableId());
-            byte[] data = page.toBinary(table);
-            int[] pageLocations = table.getPageLocations();
-            int index = -1;
+        // try (RandomAccessFile fileOut = new RandomAccessFile(Main.getDBLocation() + 
+        //         "/tables/" + page.getTableId() + ".bin", "rw")) {
+        //     Table table = Main.getCatalog().getTable(page.getTableId());
+        //     byte[] data = page.toBinary(table);
+        //     int[] pageLocations = table.getPageLocations();
+        //     int index = -1;
             
-            for (int i = 0; i < table.getPageCount(); i++) {
-                if (pageLocations[i] == page.getPageId()) {
-                    index = i;
-                    break;
-                }
-            }
+        //     for (int i = 0; i < table.getPageCount(); i++) {
+        //         if (pageLocations[i] == page.getPageId()) {
+        //             index = i;
+        //             break;
+        //         }
+        //     }
             
-            if (index < 0) {
-                System.err.println("Can't write page: No pages in table");
-                return;
-            }
+        //     if (index < 0) {
+        //         System.err.println("Can't write page: No pages in table");
+        //         return;
+        //     }
             
-            fileOut.seek(Integer.BYTES + (index * Main.getPageSize()));
-            fileOut.write(data);
-            System.out.println("Page data saved in binary format at " + fileOut);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //     fileOut.seek(Integer.BYTES + (index * Main.getPageSize()));
+        //     fileOut.write(data);
+        //     System.out.println("Page data saved in binary format at " + fileOut);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
     }
 
     public void writeBufferToHardware() {
-        Catalog catalog = Main.getCatalog();
-        byte[] tableUpdatedArray = new byte[catalog.getTableCount()];
-        Arrays.fill(tableUpdatedArray, (byte) 0);
+        //Call the storage manager to write the buffer to hardware
 
-        for (Map.Entry<PageKey, Page> entry : pages.entrySet()) {
-            Page page = entry.getValue();
-            int tableNum = page.getTableId();
+
+        // Move this functionality to StorageManager
+        // Catalog catalog = Main.getCatalog();
+        // byte[] tableUpdatedArray = new byte[catalog.getTableCount()];
+        // Arrays.fill(tableUpdatedArray, (byte) 0);
+
+        // for (Map.Entry<PageKey, Page> entry : pages.entrySet()) {
+        //     Page page = entry.getValue();
+        //     int tableNum = page.getTableId();
             
-            if (tableUpdatedArray[tableNum] == 0) {
-                Table table = catalog.getTable(tableNum);
-                try (RandomAccessFile fileOut = new RandomAccessFile(Main.getDBLocation() + 
-                        "/tables/" + tableNum + ".bin", "rw")) {
-                    fileOut.write(table.getPageCount());
-                    tableUpdatedArray[tableNum] = 1;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        //     if (tableUpdatedArray[tableNum] == 0) {
+        //         Table table = catalog.getTable(tableNum);
+        //         try (RandomAccessFile fileOut = new RandomAccessFile(Main.getDBLocation() + 
+        //                 "/tables/" + tableNum + ".bin", "rw")) {
+        //             fileOut.write(table.getPageCount());
+        //             tableUpdatedArray[tableNum] = 1;
+        //         } catch (IOException e) {
+        //             e.printStackTrace();
+        //         }
+        //     }
             
-            writePageToHardware(page);
-        }
+        //     writePageToHardware(page);
+        // }
     }
 
     public void updatePage(Page targetPage) {
